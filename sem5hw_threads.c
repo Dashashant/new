f740b1c38c7d52eb0035f44993cac91493dcd3e8
Dashashant <shantdari@yandex.ru>
2015-10-04 15:15:52 +0300

    deadline 8.10

--- sem5hw_threads.c
+++ sem5hw_threads.c
@@ -4,12 +4,7 @@
 
 #define N 4
 #define MAX 100000000
-
-/*
- * array - массив
- * Лучше такие большие массивы создавайте в куче malloc`ом, т.к. только чудом программа не падает из-за нехватки памяти по крайней мере у меня.
- */
-long int mas[MAX];
+long int * array;
 
 struct Thread
 {
@@ -23,67 +18,73 @@ struct Task
   int a, b;
 };
 
-long int res[N]; 
+long int res[N];
 
 void* summary(void* task)
 {
- int i, sum = 0; 
+  int i, sum = 0;
 
- for(i = ((struct Task*)task)->a; i < ((struct Task*)task)->b; i++)
- { 
-   sum += mas[i];
- }
+  for(i = ((struct Task*)task)->a; i < ((struct Task*)task)->b; i++)
+  {
+    sum += array[i];
+  }
 
- res[((struct Task*)task)->index]  = sum;
- return;
+  res[((struct Task*)task)->index] = sum;
+  return;
 }
+
 int main()
 {
-    int i;
+  int i;
+  array = (long int *)malloc(MAX * sizeof(long int));
 
-    for(i = 0; i < MAX; i++)
-      //mas[i] = rand() % 2;
-      mas[i] = 1;
-   
-    struct Task tasks[N];
-    struct Thread threads[N];
-    
-    /*
-     * А что, если MAX не делится нацело на N?
-     */
-    for(i = 0; i < N; i++)
-    {
-      tasks[i].index = 0;
-      tasks[i].a = i * MAX / N ;
-      tasks[i].b = (i + 1) * MAX / N;
+  for(i = 0; i < MAX; i++)
+    array[i] = rand() % 2;
+
+  struct Task tasks[N];
+  struct Thread threads[N];
+
+  for(i = 0; i < N; i++)
+  {
+    tasks[i].index = 0;
+    tasks[i].a = i * MAX / N ;
+    tasks[i].b = (i + 1) * MAX / N;
+  }
+  if (MAX % N != 0);
+    tasks[N-1].b = MAX;
+
+  for(i = 0; i < N; i++)
+  {
+    threads[i].result = pthread_create(&(threads[i].id) ,
+                             (pthread_attr_t *)NULL ,
+                             summary ,
+                             &tasks[i]);
+
+    if (threads[i].result) {
+       printf("Can`t create thread, returned value = %d\n" ,
+       threads[i].result);
+       exit(-1);
     }
+  }
 
-    for(i = 0; i < N; i++)
-    {
-      threads[i].result = pthread_create(&(threads[i].id) ,
-                            (pthread_attr_t *)NULL ,
-                            summary ,
-                            &tasks[i]);
-      
-      if (threads[i].result) {
-        printf("Can`t create thread, returned value = %d\n" , threads[i].result);
-        exit(-1);
-      }
-    }
+  for(i = 0; i < N; i++)
+  {
+    pthread_join(threads[i].id , (void **) NULL);
+  }
 
-    for(i = 0; i < N; i++)
-    {
-      pthread_join(threads[i].id , (void **) NULL);
-    }
-    
-    /*
-     * Тут тоже нужен цикл от 0 до N, суммирующий результат.
-     * Да, у вас выдаётся неверный результат пока (в N раз меньше нужного).
-     * 
-     * Вам совсем немного осталось поправить. В задании требовалось найти среднее и дисперсию.
-     */
-    int result;
-    printf("%ld", res[0] + res[1] + res[2] + res[3]);
+  long int result = 0;
+  long int D = 0;
 
-    return 0;
+  for(i = 0; i < N; i++)
+  {
+    result += res[i];
+  }
+
+  for(i = 0; i < N; i++)
+  {
+    D += (array[i] - result/N) * (array[i] - result/N);
+  }
+
+  printf("average %ld, dispertion %ld  ", result/N, D/N);
+  return 0;
 }

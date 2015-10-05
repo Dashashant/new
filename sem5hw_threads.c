1683e7266ce4222e857cba6241ca8d1a3a5ccc72
VladMiryaha <vlad.miryaha@gmail.com>
2015-10-05 17:35:46 +0300

    ok

--- sem5hw_threads.c
+++ sem5hw_threads.c
@@ -5,6 +5,11 @@
 #define MAX 1000000
 long int * array;
 
+/*
+ * Засчитано. Вы неверно считали дисперсию, т.к. суммировали в целочисленную переменную вещественные числа.
+ * В остальном всё нормально.
+ */
+
 struct Thread
 {
   pthread_t id;
@@ -18,7 +23,7 @@ struct Task
 };
 
 long int res[N];
-long int desp_res[N];
+double disp_res[N];
 long int result = 0;
 
 void* summary(void* task)
@@ -32,19 +37,20 @@ void* summary(void* task)
 
   res[((struct Task*)task)->index] = sum;
   printf("index - %d, sum - %ld\n", ((struct Task*)task)->index, res[((struct Task*)task)->index]);
-  return;
 }
-void* dispertion(void* task)
-{
-  long int i, sum = 0;
 
+void* dispersion(void* task)
+{
+  long int i;
+  double sum = 0;
+  
   for(i = ((struct Task*)task)->a; i < ((struct Task*)task)->b; i++)
   {
     sum += (array[i] - (float)result / MAX) * (array[i] - (float)result / MAX);
   }
 
-  desp_res[((struct Task*)task)->index] = sum;
-  return;
+  disp_res[((struct Task*)task)->index] = sum;
+  printf("index - %d, disp_sum - %f\n", ((struct Task*)task)->index, disp_res[((struct Task*)task)->index]);
 }
 
  int main()
@@ -53,7 +59,9 @@ void* dispertion(void* task)
    array = (long int *)malloc(MAX * sizeof(long int));
 
    for(i = 0; i < MAX; i++)
-     array[i] = 1;
+     // array[i] = 1;
+     array[i] = rand() % 2;
+   
 
    struct Task tasks[N];
    struct Thread threads[N];
@@ -65,7 +73,7 @@ void* dispertion(void* task)
      tasks[i].b = (i + 1) * MAX / N;
    }
    if (MAX % N != 0);
-     tasks[N-1].b = MAX;
+     tasks[N - 1].b = MAX;
 
    for(i = 0; i < N; i++)
    {
@@ -73,9 +81,8 @@ void* dispertion(void* task)
                               (pthread_attr_t *)NULL ,
                               summary ,
                               &tasks[i]);
-
-
-     if (threads[i].result) {
+     if (threads[i].result) 
+     {
         printf("Can`t create thread, returned value = %d\n" ,
         threads[i].result);
         exit(-1);
@@ -92,7 +99,6 @@ void* dispertion(void* task)
    for(i = 0; i < N; i++)
    {
      printf(" %d %d interval, %ld\n", tasks[i].a, tasks[i].b, res[i]);
-
      result += res[i];
    }
 
@@ -100,26 +106,27 @@ void* dispertion(void* task)
    {
      threads[i].result = pthread_create(&(threads[i].id) ,
                               (pthread_attr_t *)NULL ,
-                              dispertion,
+                              dispersion,
                               &tasks[i]);
-
-
-     if (threads[i].result) {
+     if (threads[i].result) 
+     {
         printf("Can`t create thread, returned value = %d\n" ,
         threads[i].result);
         exit(-1);
      }
    }
-   for(i = 0; i < N; i++)
-   {
-     D += desp_res[i];
-   }
-   
+      
    for(i = 0; i < N; i++)
    {
      pthread_join(threads[i].id , (void **) NULL);
    }
 
-   printf("average %f, dispertion %f\n", (float)result / MAX, (float)D / MAX);
+   for(i = 0; i < N; i++)
+   {
+     printf("disp - %f\n", disp_res[i]);
+     D += disp_res[i];
+   }
+
+   printf("average %f, dispersion %f\n", (float)result / MAX, (float)D / MAX);
    return 0;
  }
